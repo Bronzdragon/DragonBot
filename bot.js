4e50a59705a1c33c487932fd4b7669e5a81074e0
Walter Bormans <bronzdragon@gmail.com>
2018-11-08 21:28:15 +0100

    reindent package.json file

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const config = require('./config.json');
+const {token, prefix} = require('./config.json');
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login(config.token);
+client.login(token);
